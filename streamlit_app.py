4843c124a50742d2786a5d98241f6a016ad76592
jkirchoff2022 <113204929+jkirchoff2022@users.noreply.github.com>
2023-06-27 20:52:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,6 @@ try:
     if not fruit_choice:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-    else:
 except URLError as e:
     streamlit.error()
 
