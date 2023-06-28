6320b244baebdf25fd962a1da3b4bf3222db8066
jkirchoff2022 <113204929+jkirchoff2022@users.noreply.github.com>
2023-06-27 20:15:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,6 @@ import streamlit as st
 import requests
 import snowflake.connector
 
-try:
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_cur = my_cnx.cursor()
-    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION();")
-    my_data_row = my_cur.fetchone()
-    my_cur.close()
-    my_cnx.close()
-except Exception as e:
-    print(e)
-
 streamlit.title('My Parents New Heathy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -50,8 +40,13 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # display normalized view
 streamlit.dataframe(fruityvice_normalized)
 
-conn = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = pd.read_sql("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION();", conn)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION();")
+my_data_row = my_cur.fetchone()
 
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.close()
+my_cnx.close()
